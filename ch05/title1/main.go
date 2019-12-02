fcef92960a0ed2ef7e0bcfe65c27d865309aaf61
mingzhang <mingzhang@01s-MacBook-Pro-2.local>
2019-12-02 18:54:07 +0800

    add ch05

--- ch05/title1/main.go
+++ ch05/title1/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 func main() {
-	$END$
+
+}
+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		resp.Body.Close()
+		return fmt.Errorf("%s has type %s, not text/html", url, ct)
+	}
 }
--- ch05/trace/main.go
+++ ch05/trace/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"time"
+)
+
 func main() {
-	$END$
+	bigSlowOperation()
+}
+
+func bigSlowOperation() {
+	defer trace("bigSlowOperation")()
+
+	time.Sleep(3 * time.Second)
+}
+
+func trace(msg string) func() {
+	start := time.Now()
+	log.Printf("enter %s", msg)
+	return func() {
+		log.Printf("exit %s (%s)", msg, time.Since(start))
+	}
 }
